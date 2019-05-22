12c5f4f925d1138120df494ce95e1731b6b427c5
Idongesit Jacob <iddrums1@gmail.com>
2019-05-22 14:05:04 +0100

    Select Query triangle2 Challenge Solved

---
+++ select-query-1/select-query-triangle1/select-query-triangle2/README.md
@@ -0,0 +1,2 @@
+# Select Query Triangle2
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
---
+++ select-query-1/select-query-triangle1/select-query-triangle2/triangle2.sql
@@ -0,0 +1,6 @@
+SET @number = 0;
+SELECT REPEAT('* ', @number := @number+1) FROM information_schema.tables LIMIT 20;
+
+
+SET @number = 0;
+SELECT REPEAT('* ', @number := @number+1) FROM information_schema.tables WHERE @number < 20;
